13d46e057d837a9f45091288c16f90b37145ef4a
ankush-singhal <ankush.singhal@epsilon.com>
2018-07-23 16:05:45 -0500

    fixed the variable name from citi to city

--- js/Top10Employers.js
+++ js/Top10Employers.js
@@ -42,7 +42,7 @@ d3.csv("/CS498FinalTermProject-PERMAnalysis/data/top10cities.csv", function(erro
 // Transpose the data into layers
 var dataset = d3.layout.stack()(["2014", "2015", "2016"].map(function(years) {
   return data.map(function(d) {
-    return {x: d.citi, y: +d[years]};
+    return {x: d.city, y: +d[years]};
   });
 }));
 
