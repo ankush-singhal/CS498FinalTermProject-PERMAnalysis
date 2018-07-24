cd2dcb67d262ec7098ec830acc17b1a3fa7d33d2
ankush-singhal <ankush.singhal@epsilon.com>
2018-07-24 14:52:49 -0500

    syntax update

--- js/Top10Employers.js
+++ js/Top10Employers.js
@@ -58,7 +58,7 @@ d3.csv("/CS498FinalTermProject-PERMAnalysis/data/top10cities.csv", function(d, i
 		.style("left", d3.event.pageX - 50 + "px")
                 .style("top", d3.event.pageY - 70 + "px")		
 	  	.style("display", "inline-block")
-		.html("Employer City: <b>"+d.data.city+"</b>"+ "<br>" + "Number of Employees: " +"<b>"+(d[1]-d[0])+"</b> });    			
+		.html("Employer City: <b>"+d.data.city+"</b>"+ "<br>" + "Number of Employees: " +"<b>"+(d[1]-d[0])});    			
 		});
 
   g.append("g")
