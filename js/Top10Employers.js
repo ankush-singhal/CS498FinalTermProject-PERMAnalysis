1fb8cbffb55f81fc5585b3c56b09265ee73eda66
ankush-singhal <ankush.singhal@epsilon.com>
2018-07-24 15:29:11 -0500

    update position

--- js/Top10Employers.js
+++ js/Top10Employers.js
@@ -55,8 +55,8 @@ d3.csv("/CS498FinalTermProject-PERMAnalysis/data/top10cities.csv", function(d, i
 		var xPosition = d3.mouse(this)[0] - 15;
    		var yPosition = d3.mouse(this)[1] - 25;
 	  	tooltip
-		.style("left", d3.event.pageX - 50 + "px")
-                .style("top", d3.event.pageY - 70 + "px")		
+		.style("left", d3.event.pageX+ "px")
+                .style("top", d3.event.pageY+ "px")		
 	  	.style("display", "inline-block")
 		.html("Employer City: <b>"+d.data.city+"</b>"+ "<br>" + "Number of Employees: " +"<b>"+(d[1]-d[0]));    			
 		});
