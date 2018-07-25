4e323c40d078592642f48edf5859b4fbba2ff359
ankush-singhal <ankush.singhal@epsilon.com>
2018-07-24 23:58:59 -0500

    updated bottom margin

--- js/Top10Employers.js
+++ js/Top10Employers.js
@@ -1,4 +1,4 @@
-var margin = {top: 20, right: 160, bottom: 20, left: 30};
+var margin = {top: 20, right: 160, bottom: 45, left: 30};
 
 var width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
