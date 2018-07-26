03d4106ccfb9d57988aa1250824fa7ca45c679e8
ankush-singhal <ankush.singhal@epsilon.com>
2018-07-26 00:02:55 -0500

    updated ID tooltipmap

--- js/usstates.js
+++ js/usstates.js
@@ -56,15 +56,15 @@
 		
 	uStates.draw = function(id, data, toolTip){		
 		function mouseOver(d){
-			d3.select("#tooltip").transition().duration(200).style("opacity", .9);      
+			d3.select("#tooltipmap").transition().duration(200).style("opacity", .9);      
 			
-			d3.select("#tooltip").html(toolTip(d.n, data[d.id]))  
+			d3.select("#tooltipmap").html(toolTip(d.n, data[d.id]))  
 				.style("left", (d3.event.pageX) + "px")     
 				.style("top", (d3.event.pageY - 28) + "px");
 		}
 		
 		function mouseOut(){
-			d3.select("#tooltip").transition().duration(500).style("opacity", 0);      
+			d3.select("#tooltipmap").transition().duration(500).style("opacity", 0);      
 		}
 		
 		d3.select(id).selectAll(".state")
