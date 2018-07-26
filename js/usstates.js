4bb8839e11e95638d3ff153184d2c84a1ace0f1a
ankush-singhal <ankush.singhal@epsilon.com>
2018-07-26 01:35:23 -0500

    test tooltip position

--- js/usstates.js
+++ js/usstates.js
@@ -56,11 +56,15 @@
 		
 	uStates.draw = function(id, data, toolTip){		
 		function mouseOver(d){
-			d3.select("#tooltipmap").transition().duration(200).style("opacity", .9);      
+			d3.select("#tooltipmap").transition().duration(200).style("opacity", .9); 
+			 var xPosition = d3.mouse(this)[0] - 15;
+   	 		 var yPosition = d3.mouse(this)[1] - 25;
 			
 			d3.select("#tooltipmap").html(toolTip(d.n, data[d.id]))  
-				.style("left", (d3.event.pageX) + "px")     
-				.style("top", (d3.event.pageY - 28) + "px");
+				.style("left",xPosition+ "px")
+               			.style("top", yPosition+ "px");
+				/*.style("left", (d3.event.pageX) + "px")     
+				.style("top", (d3.event.pageY - 28) + "px");*/
 		}
 		
 		function mouseOut(){
