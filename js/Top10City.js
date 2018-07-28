ef544553b582485cfb98f777ecff805c1d7f1e47
ankush-singhal <ankush.singhal@epsilon.com>
2018-07-28 01:59:13 -0500

    update test

--- js/Top10City.js
+++ js/Top10City.js
@@ -1,137 +1,77 @@
-  var svg = d3.select("#vis1"),
+var svg = d3.select("#vis1"),
     margin = {top: 20, right: 20, bottom: 20, left: 20},
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom,
     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  var x = d3.scaleBand()
+var x = d3.scaleBand()
     .rangeRound([0, width])
     .padding(0.08);
 
-//	var x1 = d3.scaleBand();
-  
-  var y = d3.scaleLinear()
+var y = d3.scaleLinear()
     .range([height, 0]);
 
-  var color = d3.scaleOrdinal()
-    .range(["#7fc97f", "#beaed4", "#fdc086", "#ffff99"]);
+var color = d3.scaleOrdinal()
+    .range(["#7fc97f", "#beaed4", "#fdc086"]);
 
-var csv = 'city,2014,2015,2016\nChicago,290,1630,2140\nHouston,335,1891,1871\nSunnyvale,385,1528,2221\nSan Francisco,313,1580,2654\nRedmond,547,739,3632\nMountain View,469,2499,3164\nSan Jose,808,2302,3116\nSanta Clara,345,3115,3588\nNew York,959,4511,5633\nCollege Station,23,7976,3636';
+var keys;
 
-var data = d3.csvParse(csv), columns = ["2014", "2015", "2016"];
-
-data.forEach(function(d) {
+d3.csv("/CS498FinalTermProject-PERMAnalysis/data/top10cities.csv", function(d, i, columns) {
     for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
     d.total = t;
-});
+    return d;
+}, function(error, data) {
+    if (error) throw error;
 
-  var keys = columns;
+    keys = data.columns.slice(1);
 
-  x.domain(data.map(function(d) { return d.city; }));
- // x1.domain(keys).rangeRound([0, x.bandwidth()]);
-  y.domain([0, d3.max(data, function(d) { return d.total; })]).nice();
-  color.domain(keys);
-  
-   var stackData = d3.stack().keys(keys)(data);
+    x.domain(data.map(function(d) { return d.city; }));
+    y.domain([0, d3.max(data, function(d) { return d.total; })]).nice();
+    color.domain(keys);
+
+    var stackData = d3.stack().keys(keys)(data);
     stackData.forEach(element => {
         var keyIdx = keys.findIndex(e => e === element.key);
         element.forEach(e2 => { e2.keyIdx = keyIdx; });
     });
 
-
-  g.append("g")
+    g.append("g")
     .selectAll("g")
-    //.data(d3.stack().keys(keys)(data))
     .data(stackData)
     .enter().append("g")
-      .attr("fill", function(d) { return color(d.key); })
+        .attr("fill", function(d) { return color(d.key); })
     .selectAll("rect")
     .data(function(d) { return d; })
     .enter().append("rect")
-      .attr("x", function(d) { return x(d.data.city); })
-      .attr("y", function(d) { return y(d[1]); })
-      .attr("height", function(d) { return y(d[0]) - y(d[1]); })
-      .attr("width", x.bandwidth())
-      .attr("stroke", "black");
-	 
-   g.append("g")
-		.attr("class", "axis")
-		.attr("transform", "translate(0," + height + ")")
-		.call(d3.axisBottom(x));
+        .attr("x", function(d) { return x(d.data.Year); })
+        .attr("y", function(d) { return y(d[1]); })
+        .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+        .attr("width", x.bandwidth())
+        .attr("stroke", "black");
 
-  g.append("g")
-		.attr("class", "axis")
-		.call(d3.axisLeft(y).ticks(null, "s"))
-		.append("text")
-		.attr("x", 2)
-		.attr("y", y(y.ticks().pop()) + 0.5)
-		.attr("dy", "0.32em")
-		.attr("fill", "#000")
-		.attr("font-weight", "bold")
-		.attr("text-anchor", "start")
-		.text("No. of Employees");
-    
-	var legend = svg.selectAll(".legend")
-      .attr("font-family", "sans-serif")
-		  .attr("font-size", 10)
-		  .attr("text-anchor", "end")
-      .data(keys.slice().reverse())
-      .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
- 
-/* var legend = g.append("g")
-		.attr("font-family", "sans-serif")
-		.attr("font-size", 10)
-		.attr("text-anchor", "end")
-		.selectAll("g")
-		.data(keys.slice().reverse())
-		.enter().append("g")
-		.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });*/
+    rect = g.selectAll("rect");
+});
 
-  legend.append("rect")
-		.attr("x", width - 19)
-		.attr("width", 19)
-		.attr("height", 19)
-		.attr("fill", color);
-
-  legend.append("text")
-		.attr("x", width - 24)
-		.attr("y", 9.5)
-		.attr("dy", "0.32em")
-		.text(function(d) { return d; });
- 
- 
-
-  rect = g.selectAll("rect");
-
-  d3.selectAll("input")
+d3.selectAll("input")
     .on("change", changed);
 
-  function changed() {
+function changed() {
     if (this.value === "stacked") transitionStep1();
     else if (this.value === "grouped") transitionStep2();
-  }
+}
 
-  function transitionStep1() {
+function transitionStep1() {
     rect.transition()
-      .attr("y", function(d) { return y(d[1]); })
-      .attr("x", function(d) { return x(d.data.city); })
-      .attr("width", x.bandwidth())
-      .attr("height", function(d) { return y(d[0]) - y(d[1]); });
-  }
-  
-	function transitionStep2() {
+    .attr("y", function(d) { return y(d[1]); })
+    .attr("x", function(d) { return x(d.data.city); })
+    .attr("width", x.bandwidth())
+    .attr("stroke", "green");
+}
+
+function transitionStep2() {
     rect.transition()
-      .attr("x", function(d,i) { 
-      return x(d.data.city) + x.bandwidth()/3*d.keyIdx; 
-      	//return x(d.data.city) + x1(d3.select(this.parentNode).datum().key);
-        //return x(d.data.city) + color(d.key);
-        //return x(d.data.city)+ x.bandwidth() / 7 * color(d.key);
-        //return x1(d.key); 
-    	})
-      //.attr("fill", function(d) { return color(d.key); })
-      .attr("width", x.bandwidth() / 3)
-      .attr("y", function(d) { return y(d[1] - d[0]); })  
-      .attr("height", function(d) { return height - y(d[1] - d[0]); });
-  }
+    .attr("x", function(d, i) { return x(d.data.city) + x.bandwidth() / (keys.length+1) * d.keyIdx; })
+    .attr("width", x.bandwidth() / (keys.length+1))
+    .attr("y", function(d) { return y(d[1] - d[0]); })
+    .attr("stroke", "red");
+}
